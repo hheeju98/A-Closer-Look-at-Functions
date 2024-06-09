b8ede410d38869cea826e974ce6ed403bb4673e1
hheeju98 <hheeju98@naver.com>
2024-06-10 00:22:54 +0900

    bind

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ const checkIn = function (flightNum, passenger) {
     flightNum = 'LH999'; // 원시형 flight변수 변화x
     passenger.name = 'Mr.' + passenger.name; //참조형 jonas객체 변화 o
     if (passenger.passport === 233423) {
-        alert('Checked in')
-    }  else {
-        alert('Wrong Passport!');
+        // alert('Checked in')
+    } else {
+        // alert('Wrong Passport!');
     }
 }
 // checkIn(flight, jonas);
@@ -145,3 +145,45 @@ book.apply(swiss, flightData);
 console.log(swiss);
 
 book.call(swiss, ...flightData);
+
+// Bind method
+// book.call(eurowings, 23, 'Sarah Williams');
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jons Schmedtmann');
+bookEW23('Martha Cooper');
+
+// with Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+    console.log(this);
+
+    this.planes++
+    console.log(this.planes);
+}
+lufthansa.buyPlane();
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+    return function (value) {
+        return value + value * RTCDataChannel;
+    }
+}
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
